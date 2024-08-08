e2bf5f4101952c6d747e562a4278b5c6572cd651
sig-veloterritoires <sig@velo-territoires.org>
2024-08-08 12:08:29 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Velodatamap'
+copyright = '2024, Vélo & Territoires'
+author = '@IdrissaD'
 
 release = '0.1'
 version = '0.1.0'
